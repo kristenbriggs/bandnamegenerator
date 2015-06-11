16a0feee3aeea3b2bf0571f9f185ebd014f324be
KRISTEN BRIGGS <kristenbriggs@gmail.com>
2015-06-10 21:05:44 -0700

    added lib

--- app/script.js
+++ app/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
 $(function() {
-  $('form').on('submit', function(event) {
+  $('form').on('submit', function(event)  {
     event.preventDefault();
     var userAdjective = $('#user-adjective').val();
-    $.post('/adjective', {word: userAdjective}, function(response) {
-      var confirm = response.message + " We saved: <em>" + response.confirm + "</em>";
-      $('#adjective-res').html(confirm);
+    $.post('/adjective', {word: userAdjective}, function()  {
+      var confirm = response.message $('#adjective-res'.html(response.message + " We saved: <em>" + response.confirm + "</em>"));
     })
   })
 
   $("button").click(function() {
-    $.get('/adjective', function(response) {
+    $.get('adjective', function(response) {
       var adjective = response.word;
       $("#adjective").text(adjective);
     });
 
-    $.get('/verb', function(response) {
+    $.get('verb', function(response) {
       var verb = response.word;
       $("#verb").text(verb);
     });
 
-    $.get('/noun', function(response) {
+    $.get('noun', function(response) {
       var noun = response.word;
       $("#noun").text(noun);
     });
+
   });
+
 });
